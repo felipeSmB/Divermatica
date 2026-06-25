a3dde922a77db997c5ba0fbc72f7c8bda074a93b
PedroeAlvaro <pp4195580@gmail.com>
2026-06-25 12:24:51 +0100

    Update conexion.php
    canexao para toda a gente

--- equiposWeb/api/conexion.php
+++ equiposWeb/api/conexion.php
@@ -10,7 +10,7 @@ $pdo = new PDO(
     $usuario,
     $password,
     [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     ]
 );
